21aabda8050cc85a1f4b57ad51b53785a35821f3
ktny <katsunori.t10@gmail.com>
2021-01-31 23:35:32 +0900

    add login/logout/review-post

--- client/src/pages/RestaurantDetail.js
+++ client/src/pages/RestaurantDetail.js
@@ -1,19 +1,26 @@
-import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
-import { getRestaurant, getRestaurantReviews } from "../api.js";
-import { Breadcrumb, Loading, Pagination, Review } from "../components";
+import { useEffect, useState } from "react"
+import { useLocation, useParams } from "react-router-dom"
+import { useAuth0 } from "@auth0/auth0-react"
+import {
+  getRestaurant,
+  getRestaurantReviews,
+  postRestaurantReview,
+} from "../api.js"
+import { Breadcrumb, Loading, Pagination, Review } from "../components"
 
 function Form({ onSubmit }) {
+  const { isAuthenticated } = useAuth0()
+
   async function handleFormSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     if (onSubmit) {
       const record = {
         title: event.target.elements.title.value,
         comment: event.target.elements.comment.value,
-      };
-      event.target.elements.title.value = "";
-      event.target.elements.comment.value = "";
-      onSubmit(record);
+      }
+      event.target.elements.title.value = ""
+      event.target.elements.comment.value = ""
+      onSubmit(record)
     }
   }
 
@@ -23,7 +30,12 @@ function Form({ onSubmit }) {
         <div className="control">
           <label className="label">タイトル</label>
           <div className="control">
-            <input name="title" className="input" required disabled />
+            <input
+              name="title"
+              className="input"
+              required
+              disabled={!isAuthenticated}
+            />
           </div>
         </div>
       </div>
@@ -31,20 +43,29 @@ function Form({ onSubmit }) {
         <div className="control">
           <label className="label">コメント</label>
           <div className="control">
-            <textarea name="comment" className="textarea" required disabled />
+            <textarea
+              name="comment"
+              className="textarea"
+              required
+              disabled={!isAuthenticated}
+            />
           </div>
         </div>
       </div>
       <div className="field">
         <div className="control">
-          <button type="submit" className="button is-warning" disabled>
+          <button
+            type="submit"
+            className="button is-warning"
+            disabled={!isAuthenticated}
+          >
             レビューを投稿
           </button>
         </div>
         <p className="help">ログインが必要です。</p>
       </div>
     </form>
-  );
+  )
 }
 
 function Restaurant({ restaurant, reviews, page, perPage }) {
@@ -81,7 +102,7 @@ function Restaurant({ restaurant, reviews, page, perPage }) {
             </div>
             <div className="block">
               {reviews.rows.map((review) => {
-                return <Review key={review.id} review={review} />;
+                return <Review key={review.id} review={review} />
               })}
             </div>
             <div className="block">
@@ -96,33 +117,48 @@ function Restaurant({ restaurant, reviews, page, perPage }) {
         )}
       </div>
     </>
-  );
+  )
 }
 
 export function RestaurantDetailPage() {
-  const [restaurant, setRestaurant] = useState(null);
-  const [reviews, setReviews] = useState(null);
+  const [restaurant, setRestaurant] = useState(null)
+  const [reviews, setReviews] = useState(null)
 
-  const params = useParams();
-  const location = useLocation();
-  const query = new URLSearchParams(location.search);
-  const perPage = 5;
-  const page = +query.get("page") || 1;
+  const { getAccessTokenWithPopup } = useAuth0()
+
+  const params = useParams()
+  const location = useLocation()
+  const query = new URLSearchParams(location.search)
+  const perPage = 5
+  const page = +query.get("page") || 1
 
   useEffect(() => {
     getRestaurant(params.restaurantId).then((data) => {
-      setRestaurant(data);
-    });
-  }, [params.restaurantId]);
+      setRestaurant(data)
+    })
+  }, [params.restaurantId])
 
   useEffect(() => {
     getRestaurantReviews(params.restaurantId, {
       limit: perPage,
       offset: (page - 1) * perPage,
     }).then((data) => {
-      setReviews(data);
-    });
-  }, [params.restaurantId, page]);
+      setReviews(data)
+    })
+  }, [params.restaurantId, page])
+
+  async function handleFormSubmit(record) {
+    await postRestaurantReview(
+      params.restaurantId,
+      record,
+      getAccessTokenWithPopup
+    )
+    const data = await getRestaurantReviews(params.restaurantId, {
+      limit: perPage,
+      offset: (page - 1) * perPage,
+    })
+    setReviews(data)
+  }
 
   return (
     <>
@@ -150,8 +186,8 @@ export function RestaurantDetailPage() {
         />
       )}
       <div className="box">
-        <Form />
+        <Form onSubmit={handleFormSubmit} />
       </div>
     </>
-  );
+  )
 }
